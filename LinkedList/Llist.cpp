a06d612a260cf54053e880387eeff1762e98a701
jeeth <jeethjayakumar@gmail.com>
2019-07-25 01:42:41 -0400

    Linked List funcs updated... Still to complete

--- LinkedList/Llist.cpp
+++ LinkedList/Llist.cpp
@@ -8,8 +8,10 @@ class Node
 public:
 	Node(int data = -1, Node *nxt_p = nullptr);
 	~Node();
-	void createNode(int data, Node *nxt);
 	int getData();
+	void setData(int &data);
+	void setNxtp(Node *nxt);
+	Node* getNxtp();
 };
 
 class Llist
@@ -38,7 +40,28 @@ Node::Node(int data, Node *nxt_p)
 
 Node::~Node()
 {
+  	delete nxt_p;
 	nxt_p = nullptr;
+}
+
+int Node::getData()
+{
+	return data;
+}
+
+void Node::setData(int &data)
+{
+	this->data = data;
+}
+
+void Node::setNxtp(Node *nxt)
+{
+	nxt_p = nxt;
+}
+
+Node* Node::getNxtp()
+{
+	return nxt_p;
 }
 
 Llist::Llist()
@@ -51,27 +74,126 @@ Llist::Llist()
 
 Llist::~Llist()
 {
-
+	delete head;
+	delete curr;
+	delete tail;
+	head = curr = tail = nullptr;	
 }
 
 void Llist::insertNodeBeg(int data)
 {
-
+	Node *temp = new Node(data, nullptr);	
+	
+	if (head == nullptr)
+	{
+		head = curr = tail = temp;
+	}
+	else
+	{
+		temp->setNxtp(head);
+		head = temp;
+	}
 }
 
 void Llist::insertNodeEnd(int data)
 {
+	Node *temp = new Node(data, nullptr);
 
+	if (head == nullptr)
+	{
+		head = curr = tail = temp;
+	}
+	else
+	{
+		tail->setNxtp(temp);
+		tail = temp;
+	}
 }
 
 void Llist::deleteNodebyNum(int data)
 {
+	bool deleted = false;
+	
+	if (head->getData() == data)
+	{
+		head = head->getNxtp();
+		deleted = true;	
+	}
+	else
+	{
+		curr = head->getNxtp();
+		while (curr != nullptr)
+		{
+			if (curr->getData() == data)
+			{
+				Node *temp = curr->getNxtp();
+				curr->setData(temp->getData());
+				curr->setNxtp(temp->getNxtp());
+				delete temp;
+				temp = nullptr;
+				deleted = true;	
+				break;
+			}
+			else
+				curr = curr->getNxtp();	
+		}
+	}
 
+	if (deleted == true)
+        	cout<<"Number "<<data<<" deleted from the list\n";	
+	else
+		cout<<"Number "<<data<<" not found in list to delete!!!\n";
 }
 
 void Llist::deleteNodebyIdx(int idx)
 {
+        bool deleted = false;
 
+	if (idx <= 0)
+	{
+		cout<<"Invalid index "<<idx<<" !!! Index value starts with 1 ...\n";
+		return ;
+	}
+	
+	if (idx == 1) // Deleting the first node
+	{
+		head = head->getNxtp();
+		deleted = true;	
+	}
+	else
+	{
+		curr = head->getNxtp();
+		int count = 2;
+		
+		while (curr != nullptr)
+		{
+			if (count == idx)
+			{
+				Node* temp = curr->getNxtp();
+				curr->setData(temp->getData());
+				curr->setNxtp(temp->getNxtp());
+				delete temp;
+				temp = nullptr;
+				deleted = true;
+				break;	
+			}
+			else
+			{
+				curr = curr->getNxtp();
+				count++;
+			}
+
+		}	
+	}
+
+	if (deleted == true)
+	{
+		cout<<"Element of idx "<<idx<<" is deleted!!!\n";
+	}
+	else
+	{
+		cout<<"List not long enough!!! Length of the current list is "<<count<<"\n";
+	}
 }
 
 int Llist::searchNode(int data)
